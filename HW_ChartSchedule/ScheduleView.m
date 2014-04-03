243e75f38dd36b2b8d637a5d05eba373fee6b234
akhakimzyanov <akhakimzyanov@gmail.com>
2014-04-03 11:32:13 +0000

    Done!

--- HW_ChartSchedule/ScheduleView.m
+++ HW_ChartSchedule/ScheduleView.m
@@ -13,22 +13,36 @@
 // количество элементов может менятся, ваша задача раcположить их равномерно
 - (id)initWithData:(NSArray*)values
 {
-    self = [super initWithFrame:CGRectMake(0, 0, 300, 400)];
-    if (self) {
-        // example
-        CGRect frame = CGRectMake(0, 0, self.frame.size.width, self.frame.size.height);
-        UILabel *label = [[UILabel alloc] initWithFrame:frame];
-        label.text = @"На этом месте\n могла быть\n ваша реклама";
-        label.backgroundColor = [UIColor clearColor];
-        label.numberOfLines = 0;
-        label.autoresizingMask = UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth;
-        [self addSubview:label];
-        
+    self = [super initWithFrame:CGRectMake(0, 0, 100, 100)];
+    self.autoresizingMask = UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth;
+    
+    UIView *left = [[UIView alloc] initWithFrame:CGRectMake(4, 4, 32, 92)];
+    left.autoresizingMask = UIViewAutoresizingFlexibleHeight;
+    for(int i = 0; i < 5; i++) {
+        UILabel *labels = [[UILabel alloc] initWithFrame:CGRectMake(0, 19.5*(4-i), 30, 15)];
+        labels.autoresizingMask = UIViewAutoresizingFlexibleTopMargin | UIViewAutoresizingFlexibleBottomMargin;
+        [labels setText:[NSString stringWithFormat:@"%d", i*25]];
+        [left addSubview:labels];
+        if (i == 4) {
+            labels.autoresizingMask = UIViewAnimationTransitionNone;
+        }
     }
+    [self addSubview:left];
+    
+    UIView *right = [[UIView alloc] initWithFrame:CGRectMake(40, 5, 56, 91)];
+    right.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+    [right setBackgroundColor:[UIColor whiteColor]];
+    size_t width = 56 / (values.count + 1);
+    for(int i = 0; i < values.count; i++) {
+        int value = [[values objectAtIndex:i] integerValue];
+        UIView *graphs = [[UIView alloc] initWithFrame:CGRectMake((i+1)*width, 91-value*0.91, 5, value*0.91)];
+        [graphs setBackgroundColor:[UIColor greenColor]];
+        graphs.autoresizingMask = UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleRightMargin | UIViewAutoresizingFlexibleTopMargin;
+        [right addSubview:graphs];
+    }
+    [self addSubview:right];
+    
     return self;
 }
-
-
-
 
 @end
