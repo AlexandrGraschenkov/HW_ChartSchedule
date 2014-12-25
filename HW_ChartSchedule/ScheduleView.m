4d95bb0a80fa9ba3090d75baf9e5781f457f25b6
Zalyalov123 <Zalyalov.Ild@gmail.com>
2014-12-25 22:49:22 +0400

    Update ScheduleView.m
    done

--- HW_ChartSchedule/ScheduleView.m
+++ HW_ChartSchedule/ScheduleView.m
@@ -23,7 +23,50 @@
         label.numberOfLines = 0;
         label.autoresizingMask = UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth;
         [self addSubview:label];
+        NSMutableArray *lables = [NSMutableArray new];
+        CGRect frame = CGRectMake(50, 10, 240, 360);
+        UIView *view = [UIView new]; 
+        view.backgroundColor = [UIColor whiteColor];
+        view.autoresizingMask = UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth;
         
+        NSArray *array = [[NSArray alloc] initWithObjects:@"0", @"10",@"20",@"30",@"60",@"100", nil];
+        double count = 1; 
+        int countOfObjects = 0;
+        while(countOfObjects < 6 ){
+        
+         UILabel *newlabel = [[UILabel alloc] initWithFrame:CGRectMake(15, frame.size.height*count, 40, 20)];
+            NSString *make =[NSString stringWithFormat: @"%@-", array[countOfObjects]];
+            newlabel.text = make;
+            newlabel.autoresizingMask = UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleBottomMargin | UIViewAutoresizingFlexibleTopMargin;
+            count = count - 0.25f;
+            [lables addObject:newlabel];
+           countOfObjects++;
+        }
+     float distance = frame.size.width / (values.count * 2 + 1);
+        NSMutableArray *viewArray = [[NSMutableArray alloc] init];
+     
+     for (int i = 0; i < values.count; i++) {
+           float value = [[values objectAtIndex:i] floatValue];
+            float x = distance * (i * 2 + 1) + 50;
+            float y = frame.size.height - (value * frame.size.height / 100) + 10;
+            float width = distance;
+            float height = value * frame.size.height / 100;
+            UIView *stlView = [[UIView alloc] initWithFrame:CGRectMake(x, y, width, height)];
+            
+            stlView.backgroundColor = [UIColor greenColor];
+            stlView.autoresizingMask = UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleRightMargin | UIViewAutoresizingFlexibleTopMargin;
+            [viewArray addObject:stlView];
+            
+            }
+            [self addSubview: view];
+            
+            for(UIView *newView in viewAray){
+            [self addSubview: newView];
+        }
+    for (UILabel *i in lables){
+        [self addSubview: i];
+    }
+    
     }
     return self;
 }
