de93c083f42629f024e10cdcb69bb8d4ae0fa130
Aynur Dinmukhametov <kukmorcity2@gmail.com>
2014-12-27 23:30:14 +0300

    HW_chartschedule

--- HW_ChartSchedule/ScheduleView.m
+++ HW_ChartSchedule/ScheduleView.m
@@ -10,25 +10,57 @@
 
 @implementation ScheduleView
 
-// количество элементов может менятся, ваша задача раcположить их равномерно
 - (id)initWithData:(NSArray*)values
 {
     self = [super initWithFrame:CGRectMake(0, 0, 300, 400)];
     if (self) {
-        // example
-        CGRect frame = CGRectMake(0, 0, self.frame.size.width, self.frame.size.height);
-        UILabel *label = [[UILabel alloc] initWithFrame:frame];
-        label.text = @"На этом месте\n могла быть\n ваша реклама";
-        label.backgroundColor = [UIColor clearColor];
-        label.numberOfLines = 0;
-        label.autoresizingMask = UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth;
-        [self addSubview:label];
-        
+        NSMutableArray *lables = [[NSMutableArray alloc] init];
+        CGRect frame = CGRectMake(50, 10, 240, 360);
+        UIView *view = [[UIView alloc] initWithFrame:frame];
+        view.autoresizingMask = UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth;
+        view.backgroundColor = [UIColor whiteColor];
+        NSArray *mass = [[NSArray alloc] initWithObjects:@"0", @"25", @"50", @"75", @"100", nil];
+        NSMutableArray *views = [[NSMutableArray alloc] init];
+        NSMutableArray *lablesA = [[NSMutableArray alloc] init];
+        double count = 1;
+        float distance = frame.size.width / (values.count * 2 + 1);
+        for (int j=0;j<5;j++){
+            UILabel *lable = [[UILabel alloc] initWithFrame:CGRectMake(10, frame.size.height*count, 40, 20)];
+            NSString *s =[NSString stringWithFormat:mass[j]];
+            lable.text = s;
+            lable.autoresizingMask = UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleBottomMargin | UIViewAutoresizingFlexibleTopMargin;
+            count = count - 0.25f;
+            [lables addObject:lable];
+        }
+        for (int i = 0; i < values.count; i++) {
+            float value = [[values objectAtIndex:i] floatValue];
+            float x = distance * (i * 2 + 1) + 50;
+            float y = frame.size.height - (value * frame.size.height / 100) + 10;
+            float width = distance;
+            float height = value * frame.size.height / 100;
+            UIView *stlView = [[UIView alloc] initWithFrame:CGRectMake(x, y, width, height)];
+            stlView.backgroundColor = [UIColor blackColor];
+            stlView.autoresizingMask = UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleRightMargin | UIViewAutoresizingFlexibleTopMargin;
+            [views addObject:stlView];
+            UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(x, 375, 125, 20)];
+            NSString *s;
+            s =[NSString stringWithFormat:@"t№%d", i];
+            label.text = s;
+            label.textColor=[UIColor whiteColor];
+            label.autoresizingMask = UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleRightMargin | UIViewAutoresizingFlexibleTopMargin;
+            [lablesA addObject:label];
+        }
+        [self addSubview:view];
+        for (UIView *stlView in views) {
+            [self addSubview:stlView];
+        }
+        for(UILabel *s in lablesA){
+            [self addSubview: s];
+        }
+        for (UILabel *s in lables) {
+            [self addSubview:s];
+        }
     }
     return self;
 }
-
-
-
-
 @end
